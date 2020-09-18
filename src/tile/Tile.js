cdefb6809987a3de260fadfb644e3b556fd18128
Andy West <andyjwest@gmail.com>
2020-09-18 06:26:21 -0400

    adding storybook

---
+++ src/tile/Tile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {string} from "prop-types";
+
+const Tile = ({props})=> <div>
+
+</div>
+
+Tile.propTypes = {
+    image: string,
+    title: string,
+    review: string
+}
+
+export default Tile
